46acfc1f8ac6064afa3c62c9cb581cf078a13054
tpmotswagole <90335451+tpmotswagole@users.noreply.github.com>
2023-02-14 14:59:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 5000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Attrition\n');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log (
+  'listening song on port 5000'))
